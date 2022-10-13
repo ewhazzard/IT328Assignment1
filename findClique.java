4845b22f22ffabce1ae3d69ce3f3038f3c582434
Evan Hazzard <ewhazzard@gmail.com>
2022-10-12 21:27:40 -0500

    Added more steps to finding clique. Still not working

--- findClique.java
+++ findClique.java
@@ -10,40 +10,26 @@ import java.util.*;
 public class findClique {
    
     // Given the compliment of a graph, call findVCover and return the vertex cover
-    public static void constructClique(int[][] adjMatrix, int vertexCount, int edgeCount, int counter) {
+    public static ArrayList<Integer> constructVCover(int[][] adjMatrix, int vertexCount, int edgeCount) {
         // Start k at the number of vertices, the maximum clique
         int k = vertexCount;
-        // Initialize timing variables
-        long startTime = 0;
-        long endTime = 0;
         // Initialize clique variables to find min clique
-        ArrayList<Integer> clique = findVCover.findVertexCover(adjMatrix, k);
-        ArrayList<Integer> priorClique = new ArrayList<Integer>();
+        ArrayList<Integer> vertexCover = findVCover.findVertexCover(adjMatrix, k);
+        ArrayList<Integer> priorVCover = new ArrayList<Integer>();
+
         // While the clique is not empty, decrease k, find a VCover of size k for adjMatrix and save it to clique. Once we break this loop we will go back to the last answer because that is the min vcover
-        while(!clique.isEmpty() && k > 0){
-            startTime = System.nanoTime();
-            priorClique = clique;
-            clique = findVCover.findVertexCover(adjMatrix, k);
-            endTime = System.nanoTime();
+        while(!vertexCover.isEmpty() && k > 0){
+            priorVCover = vertexCover;
+            vertexCover = findVCover.findVertexCover(adjMatrix, k);
             k--;
-            System.out.println(clique);
         }
 
-        // Save minimum clique
-        clique = priorClique;
-        k = clique.size();
+        // Save minimum vertex cover
+        vertexCover = priorVCover;
         // Sort the clique for human reading
-        Collections.sort(clique);
+        Collections.sort(vertexCover);
 
-        long timeElapsed = (endTime - startTime) / 1000000;
-        // Print out results
-        System.out.print("G" + counter + " (" + vertexCount + ", " + edgeCount + ")");
-        System.out.print("(size = " + k + " ms = " + timeElapsed + ") {");
-        String delimiter = ", ";
-        StringJoiner joiner = new StringJoiner(delimiter);
-        clique.forEach(item -> joiner.add(item.toString()));
-        System.out.print(joiner.toString());
-        System.out.print("}\n");
+        return vertexCover;
     }
 
 
@@ -57,6 +43,9 @@ public class findClique {
             
             // Count 
             int counter = 0;
+            // Initialize timing variables
+            long startTime = 0;
+            long endTime = 0;
             System.out.println("* Max Cliques in graphs in graphs2022.txt (reduced to K-Vertex Cover) *");
             System.out.println("\t(|V|,|E|) (size, ms used) Vertex Cover");
             while (scan.hasNextInt()) {
@@ -67,6 +56,7 @@ public class findClique {
                     break;
                 }
                 int[][] adjMatrix = new int[n][n];
+                int[][] complementMatrix = new int[n][n];
 
                 int edgeCount = 0;
 
@@ -81,26 +71,62 @@ public class findClique {
                         edgeCount += next;
                     }
                 }
+
                 // Find the compliment of the given graph by flipping 0's to 1's and 1's to 0's
                 for(int i = 0; i < n; i++){
                     for(int j = 0; j < n; j++){
                         if(adjMatrix[i][j] == 0){
-                            adjMatrix[i][j] = 1;
+                            complementMatrix[i][j] = 1;
                         }
                         else {
-                            adjMatrix[i][j] = 0;
+                            complementMatrix[i][j] = 0;
                         }
                     }
                 }               
                 // set diagonal to 1 to correctly connect vertices with themselves in the complement graph
                 for (int i = 0; i < adjMatrix.length; i++) {
-                    adjMatrix[i][i] = 1;
+                    complementMatrix[i][i] = 1;
                 }
                 // Set the number of vertices to the first number read in this file 
                 int vertexCount = n;
                 // Account for symetry of the graph and vertices being connected to themselves
                 edgeCount = (edgeCount - vertexCount) / 2;
-                constructClique(adjMatrix, vertexCount, edgeCount, counter);
+
+                // Start the timer on the problem
+                startTime = System.nanoTime();
+
+                // Construct the minimum vertexCover
+                ArrayList<Integer> complementVertexCover = new ArrayList<Integer>();
+                complementVertexCover = constructVCover(complementMatrix, vertexCount, edgeCount);
+
+                ArrayList<Integer> originalVertexCover = new ArrayList<Integer>();
+                originalVertexCover = constructVCover(adjMatrix, vertexCount, edgeCount);
+
+                ArrayList<Integer> clique = new ArrayList<Integer>();
+
+                System.out.println(complementVertexCover);
+                System.out.println(originalVertexCover);
+
+                // // clique = originalVertexCover - complementVertexCover
+                // for(int i = 0; i < originalVertexCover.size(); i++){
+                //     for(int j =0; j < complementVertexCover.size(); j++){
+                //         if(originalVertexCover.get(i).equals(complementVertexCover.get(j))){
+                //             clique.add(originalVertexCover.get(i));
+                //         }
+                //     }
+                // }
+
+
+                endTime = System.nanoTime();
+                long timeElapsed = (endTime - startTime) / 1000000;
+                // Print out results
+                System.out.print("G" + counter + " (" + vertexCount + ", " + edgeCount + ")");
+                System.out.print("(size = " + clique.size() + " ms = " + timeElapsed + ") {");
+                String delimiter = ", ";
+                StringJoiner joiner = new StringJoiner(delimiter);
+                clique.forEach(item -> joiner.add(item.toString()));
+                System.out.print(joiner.toString());
+                System.out.print("}\n");
                 break;
             }
             System.out.print("***");
